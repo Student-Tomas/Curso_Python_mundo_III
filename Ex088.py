689a8e5fcaf5bfad8badd0ed0b5180d50e3aa445
Student-Tomas <tomasoliveiradealmeida@gmail.com>
2023-04-06 22:06:46 -0300

    Mundo III, up to challenge number 88

--- Ex088.py
+++ Ex088.py
@@ -1,1 +1,11 @@
-# Challenge number 088:
+# Challenge number 088: create a program that gives sugestions to bet in the lotery.
+# six different random numbers from zero to sixty, how many times the user want.
+import random, time
+num = int(input('How many times do you want to bet? '))
+for i in range(0, num):
+    print(f'Bet number {i+1}:', end=" ")
+    bet = random.sample(range(1, 61), 6)
+    print(sorted(bet))
+    time.sleep(2)
+
+
