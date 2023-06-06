f3bee90532d0174b868a275d5ac6dee2876f7d8e
Student-Tomas <120594305+Student-Tomas@users.noreply.github.com>
2023-06-06 15:31:53 -0300

    Mundo III, up to challenge number 097

--- Ex097.py
+++ Ex097.py
@@ -1,4 +1,4 @@
-# Challenge number 097. Creat a program that show anything the user wants, centralized between frames above and under.
+# Challenge number 097. Using functions, creat a program that show anything the user wants, centralized between frames above and under.
 def wright(msg):
     msg_len = len(msg) + 4
     print('~' * msg_len)
@@ -8,3 +8,5 @@ def wright(msg):
 
 # Main Program
 wright('Vou ter que aprender Python, html, css and JS')
+wright('I have to be more disciplined')
+wright("I'm sure it will worth it")
