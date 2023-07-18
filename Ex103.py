704d54129d52abe75297f52f79b5b2077da385f5
Student-Tomas <tomasoliveiradealmeida@gmail.com>
2023-07-18 11:49:36 -0300

    Mundo III, up to challenge number 103

---
+++ Ex103.py
@@ -0,0 +1,18 @@
+# Challenge number 103: create a function called "form" that receives the name of a player and how many goals
+# did he or she scored. If some information was not inserted, show unknown for the name and zero for gols.
+def form(player='<unknown>', goals=0):
+    print(f'The player {player} scored {goals} goals.')
+
+
+# Main Program
+p = str(input("What is the name of the player? "))
+g = str(input("How many goals did the player scored? "))
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if p.strip() == '':
+    form(goals=g)
+else:
+    form(p, g)
+
