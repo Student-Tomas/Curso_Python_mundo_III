f54a8e2338fe19b53e86853481b8f8042b86f738
Student-Tomas <tomasoliveiradealmeida@gmail.com>
2023-07-24 21:05:05 -0300

    Mundo III, up to challenge number 104

--- Ex103.py
+++ Ex103.py
@@ -1,4 +1,4 @@
-# Challenge number 103: create a function called "form" that receives the name of a player and how many goals
+"""# Challenge number 103: create a function called "form" that receives the name of a player and how many goals
 # did he or she scored. If some information was not inserted, show unknown for the name and zero for gols.
 def form(player='<unknown>', goals=0):
     print(f'The player {player} scored {goals} goals.')
@@ -12,6 +12,22 @@ if g.isnumeric():
 else:
     g = 0
 if p.strip() == '':
+    form(goals=g)
+else:
+    form(p, g)
+"""
+# another try:
+def form(player='<Unknon>', goals=0):
+    print(f'The player {player} scored {goals} goals this season.')
+
+# Main program
+p = str(input('What is the name of the player? '))
+g = str(input('How many goals the player socred? '))
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if p.strip() == "":
     form(goals=g)
 else:
     form(p, g)
---
+++ Ex104.py
@@ -0,0 +1,20 @@
+# Challenge number 104: creat a function that works like the input builtin function
+# include data validation in order to receive only integers numbers
+def read_int(number):
+    print(f'The number inserted by the user was: {number}')
+
+
+# Main program
+n = str(input('Insert an integer number: '))
+if n.isnumeric():
+    n = int(n)
+elif n.isalpha():
+    while True:
+        print(f'Error! insert only numbers')
+        n = str(input('Insert an integer number: '))
+        if n.isnumeric():
+            break
+elif n.strip() == "":
+    n = 'none'
+read_int(n)
+
